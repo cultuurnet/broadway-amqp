194c0841e19be091f4bf5ecb344e0f3a59bd781c
Kristof Coomans <kristof@2dotstwice.be>
2016-03-24 12:37:01 +0100

    III-844: Add DomainMessageJSONDeserializer

---
+++ src/DomainMessageJSONDeserializer.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * @file
+ */
+
+namespace CultuurNet\BroadwayAMQP;
+
+use Broadway\Domain\DateTime;
+use Broadway\Domain\DomainMessage;
+use Broadway\Domain\Metadata;
+use Broadway\Serializer\SerializableInterface;
+use CultuurNet\Deserializer\JSONDeserializer;
+use ValueObjects\String\String as StringLiteral;
+
+class DomainMessageJSONDeserializer extends JSONDeserializer
+{
+    /**
+     * Fully qualified class name of the payload. This class should implement
+     * Broadway\Serializer\SerializableInterface.
+     * 
+     * @var $payloadClass
+     */
+    private $payloadClass;
+
+    /**
+     * @param string $payloadClass
+     */
+    public function __construct($payloadClass)
+    {
+        parent::__construct(true);
+        
+        if (!in_array(SerializableInterface::class, class_implements($payloadClass))) {
+            throw new \InvalidArgumentException(
+                sprintf(
+                    'Class \'%s\' does not implement ' . SerializableInterface::class,
+                    $payloadClass
+                )
+            );
+        }
+
+        $this->payloadClass = $payloadClass;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function deserialize(StringLiteral $data)
+    {
+        $data = parent::deserialize(
+            $data
+        );
+
+        $payloadClass = $this->payloadClass;
+
+        return new DomainMessage(
+            $data['id'],
+            $data['playhead'],
+            Metadata::deserialize($data['metadata']),
+            $payloadClass::deserialize($data['payload']),
+            DateTime::fromString($data['recorded_on'])
+        );
+    }
+}
