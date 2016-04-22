2d51b3f926ad98d94bd0ef3ca5bab807c55286ed
Luc Wollants <luc.wollants@apptite.be>
2016-04-22 09:08:06 +0200

    III-905 Fixed unit test for AMQPPublisherDecorator.

--- tests/AMQPPublisherDecoratorTest.php
+++ tests/AMQPPublisherDecoratorTest.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace CultuurNet\BroadwayAMQP;
+
 use Broadway\Domain\DomainMessage;
 use Broadway\Domain\Metadata;
 use Broadway\Domain\DateTime as BroadwayDateTime;
@@ -10,32 +11,30 @@ use Broadway\EventHandling\EventListenerInterface as AMQPPublisherInterface;
 class AMQPPublisherDecoratorTest extends \PHPUnit_Framework_TestCase
 {
     /**
-     * @var AMQPPublisherInterface
+     * @var AMQPPublisherInterface|\PHPUnit_Framework_MockObject_MockObject
      */
     private $amqpPublisher;
-    
-    /**
-     * @var DomainMessage
-     */
-    private $domainMessage;
 
     /**
      * @var AMQPPublisherDecorator
      */
     private $amqpPublisherDecorator;
+
+    /**
+     * @var DomainMessage
+     */
+    private $domainMessage;
     
     
     protected function setUp()
     {
-        //$this->amqpPublisherDecorator = $this->getMock(AMQPPublisherDecorator::class, ['handle'], [], 'AMQPPublisherDecorator', false);
-        $this->amqpPublisherDecorator = $this
-            ->getMockBuilder(AMQPPublisherDecorator::class)
-            ->setMethods(null)
-            ->getMockForAbstractClass();
-            //->disableOriginalConstructor()
-        // TODO: still gives error: Argument 1 passed to CultuurNet\BroadwayAMQP\AMQPPublisherDecorator::__construct() must implement interface Broadway\EventHandling\EventListenerInterface, null given
-        $this->amqpPublisher = $this->getMockBuilder(AMQPPublisherInterface::class)->getMock();
-        //$this->amqpPublisherDecorator->amqpPublisher = $this->amqpPublisher; // nope :(
+        $this->amqpPublisher = $this->getMock(AMQPPublisherInterface::class);
+
+        $this->amqpPublisherDecorator = $this->getMockForAbstractClass(
+            AMQPPublisherDecorator::class,
+            [$this->amqpPublisher]
+        );
+
         $this->domainMessage = new DomainMessage(
             'F68E71A1-DBB0-4542-AEE5-BD937E095F74',
             2,
@@ -51,9 +50,10 @@ class AMQPPublisherDecoratorTest extends \PHPUnit_Framework_TestCase
     /**
      * @test
      */
-    public function it_should_call_the_handle_method_of_AMQPPublisherInterface()
+    public function it_calls_handle_method_from_injected_interface()
     {
         $this->amqpPublisher->expects($this->once())->method('handle');
+
         $this->amqpPublisherDecorator->handle($this->domainMessage);
     }
 }
